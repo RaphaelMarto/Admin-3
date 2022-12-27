17b72cd249429bbc2d28d32e4daf47322e64cfe2
Lopidurs <62069633+Lopidurs@users.noreply.github.com>
2022-12-27 17:44:07 +0100

    Create disable.ps1

---
+++ disable.ps1
@@ -0,0 +1,19 @@
+Import-Module ActiveDirectory
+
+$users = Import-Csv -Path 'users.csv' -Header "username"
+foreach ($user in $users) {
+    $username = $user.username
+
+    if (Get-ADUser -Filter {SamAccountName -eq $username}) {
+        try {
+            Disable-ADAccount -Identity (Get-ADUser -Filter {SamAccountName -eq $username})
+            Write-Host "$username à bien été désactivé"
+        }
+        catch {
+            Write-Host "Une erreur s'est produite lors de la désactivation de $username"
+        }
+    }
+    else {
+        Write-Host "Il n'y a pas d'utilisateur $username"
+    }
+}
