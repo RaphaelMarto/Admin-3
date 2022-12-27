dbacd980374b914e1e489a4ce629dbcf0a86172b
Lopidurs <62069633+Lopidurs@users.noreply.github.com>
2022-12-27 17:58:15 +0100

    Update add.ps1

--- add.ps1
+++ add.ps1
@@ -10,11 +10,11 @@ foreach ($user in $users) {
     $password = $user.password
 
     if (Get-ADUser -Filter {SamAccountName -eq $username}) {
-        Write-Host "Il existe déjà un utilisateur $username"
+        Write-Host "Il existe deja  un utilisateur $username"
     }
     else {
-        $ou = 'ou=Utlisateurs ,dc=L2-4,dc=com'
-        New-ADUser -SamAccountName $username -GivenName $first_name -Surname $last_name -DisplayName $full_name -Path $ou -AccountPassword (ConvertTo-SecureString -AsPlainText $password -Force) -Enabled $true
-        Write-Host "$username à bien été créer"
+        $ou = 'ou=Utilisateurs,dc=L2-4,dc=lab'
+        New-ADUser -Name $full_name -SamAccountName $username -GivenName $first_name -Surname $last_name -DisplayName $full_name -Path $ou -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force)
+        Write-Host "$username a  bien ete creer"
     }
 }
