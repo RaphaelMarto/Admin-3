0cace267c8cd0344691dfd68e7e37f66060e8872
Lopidurs <62069633+Lopidurs@users.noreply.github.com>
2022-12-27 15:16:17 +0100

    Update ajout.py

--- ajout.py
+++ ajout.py
@@ -1,20 +1,35 @@
 import csv
-import subprocess
+import pyad
 
+# Ouvrez le fichier CSV et créez un lecteur CSV
 with open('utilisateurs.csv', 'r') as f:
-# Créez un objet CSV reader
-reader = csv.reader(f)
-# Pour chaque ligne du fichier CSV
-for row in reader:
-    # Récupérez les valeurs du nom d'utilisateur et du mot de passe
-    username = row[0]
-    password = row[1]
+    reader = csv.reader(f)
     
-    # Vérifiez si l'utilisateur existe déjà en exécutant la commande "dsquery"
-    result = subprocess.run(["dsquery", "user", "cn=" + username], stdout=subprocess.PIPE)
-    
-    # Si l'utilisateur n'existe pas déjà
-    if result.returncode != 0:
-        # Créez le compte utilisateur en exécutant la commande "dsadd"
-        subprocess.run(["dsadd", "user", "cn=" + username, "-pwd", password])
-print("Tous les comptes utilisateurs ont été créés avec succès.")
+    # Pour chaque ligne du fichier CSV
+    for row in reader:
+        # Récupérez les informations sur l'utilisateur à partir de la ligne CSV
+        nom_utilisateur = row[0]
+        prenom_utilisateur = row[1]
+        nom_complet = f"{prenom_utilisateur} {nom_utilisateur}"
+        nom_compte = row[2]
+        mot_de_passe = row[3]
+        
+        # Vérifiez si le compte utilisateur existe déjà dans Active Directory
+        user = pyad.aduser.ADUser.from_cn(nom_compte)
+        if not user:
+            # Si le compte n'existe pas, créez-le avec pyad dans l'OU "utilisateurs"
+            user = pyad.aduser.ADUser.create(nom_compte, ou='ou=utilisateurs,dc=l2-4,dc=lab')
+            
+            # Affectez les propriétés de l'objet utilisateur
+            user.set_givenName(prenom_utilisateur)
+            user.set_surname(nom_utilisateur)
+            user.set_displayName(nom_complet)
+            user.set_userPrincipalName(f"{nom_compte}@example.com")
+            user.set_mail(f"{nom_compte}@example.com")
+            user.set_password(mot_de_passe)
+            
+            # Enregistrez les modifications de l'objet utilisateur dans Active Directory
+            user.update_changes()
+        else:
+            # Si le compte existe déjà, affichez un message d'erreur
+            print(f"Le compte {nom_compte} existe déjà dans Active Directory.")
